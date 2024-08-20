78b25f30ec45cec32e1cf1705bd0b2f555c43f8b
username <wecp-dummy@gmail.com>
2024-08-20 09:32:09 +0000

    wecp initial commit

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  // apiUrl: 'http://localhost:1000'
-  apiUrl: 'https://ec2-13-201-189-145_5000.projects.wecreateproblems.com'
+  // production: false,
+  apiUrl: window.origin.replace("3000", "5000")
 
 };
+
